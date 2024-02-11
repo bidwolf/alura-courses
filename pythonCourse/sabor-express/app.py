d4319faf6dd030c41a2df2de69ab0b2b7ba56bc2
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2024-02-11 18:13:24 -0300

    turn into english

--- pythonCourse/sabor-express/app.py
+++ pythonCourse/sabor-express/app.py
@@ -1,34 +1,39 @@
-def betweenBreakLines(str:str):
-  for letter in str:
-    print(f'{letter}\n')
+"""Module focused on python course of Alura"""
 
-print('Sabor Express\n')
+
+def between_break_lines(word: str) -> None:
+    """Print each letter of the word between break lines"""
+    for letter in word:
+        print(f"{letter}\n")
+
+
+print("Sabor Express\n")
 options = [
-  '1. Cadastrar restaurante',
-  '2. Listar restaurante',
-  '3. Ativar restaurante',
-  '4. Sair',
+    "1. Cadastrar restaurante",
+    "2. Listar restaurante",
+    "3. Ativar restaurante",
+    "4. Sair",
 ]
-betweenBreakLines(options)
-opcao_escolhida = input('Escolha uma opção:')
-print(f'Você escolheu a opção {opcao_escolhida}\n')
+between_break_lines(options)
+selected_option = input("select one option:")
+print(f"You select: {selected_option}\n")
 
-print ('Python na Escola de Programação da Alura\n')
+print("Python on Alura Programming School \n")
 
 # Usando loop
-betweenBreakLines('ALURA')
+between_break_lines("ALURA")
 
 # Usando Sep
-print('A','L','U','R','A',sep='\n')
+print("A", "L", "U", "R", "A", sep="\n")
 
-pi = 3.14159
+PI = 3.14159
 
 # Usando Round
-print(f'{round(pi, 2)}')
+print(f"{round(PI, 2)}")
 # Usando o método f-string
-print(f'{pi:.2f}')
-# Usando .format
-print('{:.2f}'.format(pi))
+print(f"{PI:.2f}")
+# Usando f-string
+print(f"{PI:.2f}")
 
-nome = 'Henrique de Paula Rodrigues'
-idade = 23
+NAME = "Henrique de Paula Rodrigues"
+AGE = 23
