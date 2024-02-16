d3d74788ec827c7cfcd58b7f6b543cdfe83911e0
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2024-02-16 05:25:51 -0300

    using challenges and app_sequential on app.py (now modularized)

--- pythonCourse/app.py
+++ pythonCourse/app.py
@@ -3,21 +3,19 @@
 from src.models.application import Application
 from src.models.menu.drink import Drink
 from src.models.menu.food import Food
-
+from src.challenges.main import run_challenges
+from src import app_sequential_version
 
 app = Application()
 
 
 def main():
     """The main Function"""
-    # clear_prompt() # version sequential
-    # show_version_menu() # version sequential
-    # app.run()
-    # print("------------------ CHALLENGES ------------------\n")
-    # for i in range(8, 9):
-    #     run_challenge(number=i)
-    # app.bump_version(bump_type="PATCH")
-    # print(app)
+    app.run()
+    app.bump_version(bump_type="PATCH")
+    print(app)
+    run_challenges()
+    app_sequential_version.main()
     bred = Food(description="sugar bred", name="Bred", price=20.00)
     lemonade = Drink(name="Bred", price=20.00, size="large")
     print(bred)
