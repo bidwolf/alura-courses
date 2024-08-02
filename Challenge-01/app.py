c7016aa080a3db0c49f07361373bbf0f64fa9c22
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2024-08-02 17:17:24 -0300

    feat: criando função para listar todos os contatos

--- Challenge-01/app.py
+++ Challenge-01/app.py
@@ -60,15 +60,28 @@ Caso não deseje alterar o campo basta apenas apertar a tecla
 <Enter> sem preencher o campo em questão\n"""
     )
     new_name = str(input("Nome do contato:(Pressione <ENTER> para pular)\n"))
-    new_phoneNumber = str(input("Telefone:(Pressione <ENTER> para pular)\n"))
+    new_phone_number = str(input("Telefone:(Pressione <ENTER> para pular)\n"))
     new_mail = str(input("Email do contato:(Pressione <ENTER> para pular)\n"))
-    if not new_name and not new_mail and not new_phoneNumber:
+    if not new_name and not new_mail and not new_phone_number:
         print("Nenhuma alteração foi feita")
         return
     contact["name"] = new_name if new_name else contact["name"]
     contact["phoneNumber"] = (
-        new_phoneNumber if new_phoneNumber else contact["phoneNumber"]
+        new_phone_number if new_phone_number else contact["phoneNumber"]
     )
     contact["mail"] = new_mail if new_mail else contact["mail"]
     print("Contato alterado com sucesso")
     return
+
+
+def show_all_contacts():
+    """
+    Função responsável por listar todos os contatos existentes
+    """
+    print("CODE\tNAME\tPHONE\tFAVORITE ")
+    for index, contact in enumerate(contacts):
+        name = contact["name"]
+        mail = contact["mail"]
+        phoneNumber = contact["phoneNumber"]
+        isFavorite = "✔️" if contact["isFavorite"] else "x"
+        print(f"{index}\t{name}\t{mail}\t{phoneNumber}\t{isFavorite}")
