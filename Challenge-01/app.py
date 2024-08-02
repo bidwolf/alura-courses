aca7db33dfe294783db42929b85a50b4e283c95e
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2024-08-02 09:56:55 -0300

    feat: adding source for Rocketseat challenge

---
+++ Challenge-01/app.py
@@ -0,0 +1,15 @@
+"""
+Este app é responsável por criar uma aplicação de terminal para cadastro de contatos.
+O mesmo segue os requisitos do desafio proposto pela Rocketseat no curso de Python.
+Acesse o link para mais informações: https://efficient-sloth-d85.notion.site/Desafio-01-622bb29769034c9ba659f2dc33019055
+"""
+
+
+def main():
+    """
+    Funcao principal do app
+    """
+    print("BEM VINDO AO ROCKET CONTACTS")
+
+
+main()
