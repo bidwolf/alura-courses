50072c92e87badb89d0468fc1d479109c5863453
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2024-08-31 11:35:24 -0300

    feat: adicionando rotas para atualizar campeões

--- flaskCourse/views.py
+++ flaskCourse/views.py
@@ -1,4 +1,4 @@
-from app import app
+from app import app, db
 from models import Champions, Users
 from src.middlewares.authentication import is_authenticated
 from flask import render_template, request, redirect, session, flash
@@ -25,11 +25,40 @@ def create_champion():
     return render_template("create_champion.html")
 
 
+@app.route("/update-champion/<int:champion_id>")
+def update_champion(champion_id):
+    """This is the route that allows the user add a favorite champion"""
+    if not is_authenticated():
+        return redirect("/login", code=422)
+    existent_champion = Champions.query.filter_by(id=champion_id).first()
+    return render_template("update_champion.html", champion=existent_champion)
+
+
+@app.route("/update", methods=["POST"])
+def update():
+    """This is the endpoint to update a champion"""
+    if not is_authenticated():
+        return redirect("/login", code=422)
+    lane = request.form["champion_lane"]
+    name = request.form["champion_name"]
+    champion_id = request.form["champion_id"]
+    rows_changed = Champions.query.filter_by(id=champion_id).update(
+        dict(lane=lane, champion_name=name)
+    )
+    if rows_changed:
+        db.session.commit()
+        flash("Campeão atualizado com sucesso!")
+        return redirect("/", code=302)
+    else:
+        db.session.flush()
+        flash("Something wrong, cannot update champion.\nPlease, try again later.")
+
+
 @app.route("/create", methods=["POST"])
 def create():
     """This is the endpoint to create a champion"""
     if not is_authenticated():
-        return redirect("/login", 422)
+        return redirect("/login", code=422)
     lane = request.form["champion_lane"]
     name = request.form["champion_name"]
     champion_already_exists = (
