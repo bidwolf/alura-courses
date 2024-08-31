01280cc81db0958da8c7c2b795cef749a244cc7f
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2024-08-31 09:46:25 -0300

    docs: updating requirements.txt

---
+++ flaskCourse/config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # This line read .env configuration
+SECRET_KEY = os.getenv("APP_SECRET_KEY")
+SQLALCHEMY_DATABASE_URI = "{SGBD}://{user}:{password}@{server}/{database}".format(
+    SGBD="mysql+mysqlconnector",
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    server=os.getenv("DB_HOST"),
+    database=os.getenv("DB_NAME"),
+)
