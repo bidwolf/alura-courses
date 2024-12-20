b5602c547cabb188e07995e19aa1c8ea6dcacfe8
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2024-12-20 11:15:57 -0300

    feat: add delete method to allow user deletion in the API

--- rocketseat-flask-auth/app.py
+++ rocketseat-flask-auth/app.py
@@ -12,6 +12,7 @@ from flask_login import (
 from database import db
 from models.users import User
 from utils import is_valid_email
+import http
 
 app = Flask(__name__)
 app.config.from_pyfile("config.py")
@@ -34,7 +35,7 @@ def unauthorized():
             "message": "You should be logged in to access this route.",
             "error": True,
         },
-        401,
+        http.HTTPStatus.UNAUTHORIZED,
     )
 
 
@@ -45,18 +46,18 @@ def bad_request(error):
             "message": "Bad request.",
             "error": True,
         },
-        400,
+        http.HTTPStatus.NOT_FOUND,
     )
 
 
-@app.errorhandler(404)
+@app.errorhandler(http.HTTPStatus.NOT_FOUND)
 def not_found(error):
     return make_response(
         {
             "message": "Content Not Found.",
             "error": True,
         },
-        404,
+        http.HTTPStatus.NOT_FOUND,
     )
 
 
@@ -70,35 +71,41 @@ def signup():
                 "message": "You should provide a username and a password to create a User.",
                 "error": True,
             },
-            400,
+            http.HTTPStatus.BAD_REQUEST,
         )
     username = data.get("username")
     password = data.get("password")
     email = data.get("email")
     if not username:
         return make_response(
-            {"message": "You should provide a username.", "error": True}, 400
+            {"message": "You should provide a username.", "error": True},
+            http.HTTPStatus.BAD_REQUEST,
         )
     if not password:
         return make_response(
-            {"message": "You should provide a password.", "error": True}, 400
+            {"message": "You should provide a password.", "error": True},
+            http.HTTPStatus.BAD_REQUEST,
         )
     if not email:
         return make_response(
-            {"message": "You should provide an email.", "error": True}, 400
+            {"message": "You should provide an email.", "error": True},
+            http.HTTPStatus.BAD_REQUEST,
         )
 
     if not isinstance(username, str):
         return make_response(
-            {"message": "The username should be a string.", "error": True}, 400
+            {"message": "The username should be a string.", "error": True},
+            http.HTTPStatus.BAD_REQUEST,
         )
     if not isinstance(password, str):
         return make_response(
-            {"message": "The password should be a string.", "error": True}, 400
+            {"message": "The password should be a string.", "error": True},
+            http.HTTPStatus.BAD_REQUEST,
         )
     if not isinstance(email, str):
         return make_response(
-            {"message": "The email should be a string.", "error": True}, 400
+            {"message": "The email should be a string.", "error": True},
+            http.HTTPStatus.BAD_REQUEST,
         )
     if len(username) < 4 or len(username) > 32:
         return make_response(
@@ -106,7 +113,7 @@ def signup():
                 "message": "The username should have between 4 and 32 characters.",
                 "error": True,
             },
-            400,
+            http.HTTPStatus.BAD_REQUEST,
         )
     if len(password) < 6 or len(password) > 32:
         return make_response(
@@ -114,11 +121,12 @@ def signup():
                 "message": "The password should have between 6 and 32 characters.",
                 "error": True,
             },
-            400,
+            http.HTTPStatus.BAD_REQUEST,
         )
     if not is_valid_email(email):
         return make_response(
-            {"message": "The email should be valid.", "error": True}, 400
+            {"message": "The email should be valid.", "error": True},
+            http.HTTPStatus.BAD_REQUEST,
         )
     if username and password and email:
         user = User.query.filter(
@@ -130,7 +138,7 @@ def signup():
                     "message": "The username is already in use.",
                     "error": True,
                 },
-                409,
+                http.HTTPStatus.CONFLICT,
             )
         if user and user.email == email:
             return make_response(
@@ -138,7 +146,7 @@ def signup():
                     "message": "The email is already in use.",
                     "error": True,
                 },
-                409,
+                http.HTTPStatus.CONFLICT,
             )
         try:
             user = User(username=username, password=password, email=email)
@@ -149,7 +157,7 @@ def signup():
                     "message": "User created successfully.",
                     "error": False,
                 },
-                201,
+                http.HTTPStatus.CREATED,
             )
         except Exception as e:
             print(e)
@@ -158,7 +166,7 @@ def signup():
                     "message": "An error occurred while creating the user.",
                     "error": True,
                 },
-                500,
+                http.HTTPStatus.INTERNAL_SERVER_ERROR,
             )
 
 
@@ -181,7 +189,7 @@ def get_user(user_id):
                     "message": "User not found.",
                     "error": True,
                 },
-                404,
+                http.HTTPStatus.NOT_FOUND,
             )
         return make_response(
             {
@@ -190,7 +198,7 @@ def get_user(user_id):
                 "error": False,
                 "message": "User found successfully.",
             },
-            200,
+            http.HTTPStatus.OK,
         )
     except Exception as e:
         print(e)
@@ -199,7 +207,7 @@ def get_user(user_id):
                 "message": "An error occurred while getting the user.",
                 "error": True,
             },
-            500,
+            http.HTTPStatus.INTERNAL_SERVER_ERROR,
         )
 
 
@@ -214,7 +222,7 @@ def update_user(user_id):
                 "message": "You should provide a data to update the user information",
                 "error": True,
             },
-            400,
+            http.HTTPStatus.BAD_REQUEST,
         )
     new_email = data.get("email")
     new_password = data.get("password")
@@ -224,7 +232,7 @@ def update_user(user_id):
                 "message": "You should provide a data to update the user information",
                 "error": True,
             },
-            400,
+            http.HTTPStatus.BAD_REQUEST,
         )
     if not isinstance(user_id, int):
         return make_response(
@@ -232,16 +240,19 @@ def update_user(user_id):
                 "message": "You should provide an integer value for user ID.",
                 "error": True,
             },
-            400,
+            http.HTTPStatus.BAD_REQUEST,
         )
     try:
         user = db.session.query(User).get(user_id)
         if not user:
-            return make_response({"message": "User not found.", "error": True}, 404)
+            return make_response(
+                {"message": "User not found.", "error": True}, http.HTTPStatus.NOT_FOUND
+            )
         if new_email and user.email != new_email:
             if not is_valid_email(new_email):
                 return make_response(
-                    {"message": "The email should be valid.", "error": True}, 400
+                    {"message": "The email should be valid.", "error": True},
+                    http.HTTPStatus.BAD_REQUEST,
                 )
             existent_email_query = (
                 db.session.query(User).filter(User.email == new_email).first()
@@ -253,7 +264,7 @@ def update_user(user_id):
                         "message": "This email is already registered by someone else, please check the email provided.",
                         "error": True,
                     },
-                    409,
+                    http.HTTPStatus.CONFLICT,
                 )
         if new_password:
             if len(new_password) < 6 or len(new_password) > 32:
@@ -262,7 +273,7 @@ def update_user(user_id):
                         "message": "The password should have between 6 and 32 characters.",
                         "error": True,
                     },
-                    400,
+                    http.HTTPStatus.BAD_REQUEST,
                 )
         user.password = new_password if new_password else user.password
         user.email = new_email if new_email else user.email
@@ -279,8 +290,36 @@ def update_user(user_id):
                 },
                 "updated_at": updated_at,
             },
-            201,
+            http.HTTPStatus.CREATED,
         )
+    except Exception as e:
+        print(e)
+        return make_response(
+            {"message": "Something went wrong while updating the user.", "error": True}
+        )
+
+
+@app.route("/user/<int:user_id>", methods=["DELETE"])
+@login_required
+def delete_user(user_id):
+    """This is the delete user route."""
+    if not isinstance(user_id, int):
+        return make_response(
+            {
+                "message": "You should provide an integer value for user ID.",
+                "error": True,
+            },
+            http.HTTPStatus.BAD_REQUEST,
+        )
+    try:
+        user = db.session.query(User).get(user_id)
+        if not user:
+            return make_response(
+                {"message": "User not found.", "error": True}, http.HTTPStatus.NOT_FOUND
+            )
+        db.session.delete(user)
+        db.session.commit()
+        return make_response("", http.HTTPStatus.NO_CONTENT)
     except Exception as e:
         print(e)
         return make_response(
@@ -298,25 +337,29 @@ def login():
                 "message": "You should provide a username and a password to login.",
                 "error": True,
             },
-            400,
+            http.HTTPStatus.BAD_REQUEST,
         )
     username = data.get("username")
     password = data.get("password")
     if not username:
         return make_response(
-            {"message": "You should provide a username.", "error": True}, 400
+            {"message": "You should provide a username.", "error": True},
+            http.HTTPStatus.BAD_REQUEST,
         )
     if not password:
         return make_response(
-            {"message": "You should provide a password.", "error": True}, 400
+            {"message": "You should provide a password.", "error": True},
+            http.HTTPStatus.BAD_REQUEST,
         )
     if not isinstance(username, str):
         return make_response(
-            {"message": "The username should be a string.", "error": True}, 400
+            {"message": "The username should be a string.", "error": True},
+            http.HTTPStatus.BAD_REQUEST,
         )
     if not isinstance(password, str):
         return make_response(
-            {"message": "The password should be a string.", "error": True}, 400
+            {"message": "The password should be a string.", "error": True},
+            http.HTTPStatus.BAD_REQUEST,
         )
     if username and password:
         user = User.query.filter_by(username=username).first()
@@ -326,7 +369,7 @@ def login():
                     "message": "Username or password invalid.",
                     "error": True,
                 },
-                401,
+                http.HTTPStatus.UNAUTHORIZED,
             )
         login_user(user)
         return make_response(
@@ -334,7 +377,7 @@ def login():
                 "message": "User logged in successfully.",
                 "error": False,
             },
-            200,
+            http.HTTPStatus.OK,
         )
 
 
@@ -347,7 +390,7 @@ def logout():
             "message": "User logged out successfully.",
             "error": False,
         },
-        200,
+        http.HTTPStatus.OK,
     )
 
 
