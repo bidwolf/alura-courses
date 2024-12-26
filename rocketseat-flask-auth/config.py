45c66b142e8e6d673c4a7a807b02788a525576ef
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2024-12-26 11:36:40 -0300

    feat: update database configuration to use MySQL with environment variables

--- rocketseat-flask-auth/config.py
+++ rocketseat-flask-auth/config.py
@@ -4,7 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()  # This line read .env configuration
 SECRET_KEY = os.getenv("APP_SECRET_KEY")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
 if not SECRET_KEY:
     raise ValueError("No SECRET_KEY set for Flask application")
-SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
+SQLALCHEMY_DATABASE_URI = (
+    f"mysql+pymysql:///{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
 UPLOAD_PATH = os.path.dirname(os.path.abspath(__file__)) + "/uploads"
