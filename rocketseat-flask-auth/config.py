bde4c27050858095826f0188ad3c1650d8ccb9b7
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2024-12-26 12:50:18 -0300

    fix: corrigir formatação da URI do banco de dados MySQL

--- rocketseat-flask-auth/config.py
+++ rocketseat-flask-auth/config.py
@@ -12,6 +12,6 @@ DB_NAME = os.getenv("DB_NAME")
 if not SECRET_KEY:
     raise ValueError("No SECRET_KEY set for Flask application")
 SQLALCHEMY_DATABASE_URI = (
-    f"mysql+pymysql:///{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 )
 UPLOAD_PATH = os.path.dirname(os.path.abspath(__file__)) + "/uploads"
