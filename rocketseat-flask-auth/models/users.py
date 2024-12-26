fb2d7237bd716c5597f1634b44d58345b2c6347a
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2024-12-26 15:43:28 -0300

    Feat/password encryption (#109)
    * Feat: adicionar arquivo de requisitos com dependências do projeto
    
    * Feat: adicionar criptografia de senha no cadastro e atualização de usuário
    
    * Feat: aumentar o tamanho dos campos de username, email, password e role, e implementar verificação de senha
    
    * Feat: adicionar funções para criptografar e verificar senhas, além de validação de email

--- rocketseat-flask-auth/models/users.py
+++ rocketseat-flask-auth/models/users.py
@@ -4,6 +4,7 @@ from sqlalchemy import Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
 from flask_login import UserMixin
 from database import db
+from utils import verify_password
 
 
 class User(db.Model, UserMixin):
@@ -11,11 +12,12 @@ class User(db.Model, UserMixin):
 
     id: Mapped[int] = mapped_column(Integer(), primary_key=True)
     username: Mapped[str] = mapped_column(
-        String(length=32), unique=True, nullable=False
+        String(length=80), unique=True, nullable=False
     )
-    email: Mapped[str] = mapped_column(String(length=64), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(String(length=32), nullable=False)
-    role: Mapped[str] = mapped_column(String(length=32), nullable=False, default="user")
+    email: Mapped[str] = mapped_column(String(length=80), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(length=80), nullable=False)
+    role: Mapped[str] = mapped_column(String(length=80), nullable=False, default="user")
 
     def check_password(self, password: str) -> bool:
-        return self.password == password
+        """Check if the given password is valid"""
+        return verify_password(encrypted_password=self.password, password=password)
--- rocketseat-flask-auth/utils.py
+++ rocketseat-flask-auth/utils.py
@@ -1,5 +1,20 @@
 """This is a module for utilities"""
+
 import re
-def is_valid_email(email:str)->bool:
+import bcrypt
+
+
+def is_valid_email(email: str) -> bool:
     """Validate a email format"""
     return re.match(r"[^@]+@[^@]+\.[^@]+", email) and len(email) <= 64
+
+
+def encrypt_password(password: str) -> bytes:
+    """Encrypt the password"""
+    hashed_password = bcrypt.hashpw(str.encode(password), bcrypt.gensalt())
+    return hashed_password
+
+
+def verify_password(password: str, encrypted_password: str) -> bool:
+    """Verify if the password matches the encrypted password"""
+    return bcrypt.checkpw(str.encode(password), str.encode(encrypted_password))
