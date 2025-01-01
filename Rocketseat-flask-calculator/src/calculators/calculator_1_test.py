dbbea0642acf8b6267652c40782b1859e99b35b3
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2025-01-01 20:02:22 -0300

    Test/first calculator (#147)
    * test: add tests for the first calculator
    - Should raise a ValueError when the body is bad formated
    - Should return a dictionary with data containing the calculator and the result of the math
    - Should result in the response dictionary being a float.
    - Should return 14.25 when the number in the request.json is 1

---
+++ Rocketseat-flask-calculator/src/calculators/calculator_1_test.py
@@ -0,0 +1,39 @@
+import pytest
+from typing import Dict
+from .calculator_1 import FirstCalculator
+
+calculator = FirstCalculator()
+
+
+class MockRequest:
+    def __init__(self, body: Dict):
+        self.json = body
+
+
+def test_calculate():
+    mocked_request = MockRequest({"number": 1})
+    response = calculator.calculate(mocked_request)
+    print()
+    print(response)
+    assert "data" in response
+    assert "calculator" in response["data"]
+    assert "result" in response["data"]
+    assert response["data"]["calculator"] == 1
+    assert isinstance(response["data"]["result"], float)
+    assert response["data"]["result"] == 14.25
+
+
+def test_calculate_without_number_in_body():
+
+    with pytest.raises(ValueError) as exception_info:
+        mocked_request = MockRequest({})
+        calculator.calculate(mocked_request)
+        assert exception_info == "Number is required!"
+
+
+def test_calculate_with_invalid_number_field():
+
+    with pytest.raises(ValueError) as exception_info:
+        mocked_request = MockRequest({"number": "a"})
+        calculator.calculate(mocked_request)
+        assert exception_info == "Number should be float or integer"
