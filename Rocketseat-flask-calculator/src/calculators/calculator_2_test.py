7ec26bc835c74b089ea840eb69329cf9a92beb66
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2025-01-02 00:40:29 -0300

    Test/second calculator (#152)
    * chore : add requirements.txt file to track project dependencies
    
    * test: create unit tests for the second calculator logic

---
+++ Rocketseat-flask-calculator/src/calculators/calculator_2_test.py
@@ -0,0 +1,71 @@
+"""This module is responsible for unit tests involving the second calculator"""
+
+from typing import Dict
+import pytest
+from src.drivers.numpy_handler import NumpyHandler
+from src.drivers.interfaces.driver_handler_interface import DriverHandlerInterface
+from .calculator_2 import SecondCalculator
+
+
+class MockRequest:
+    def __init__(self, body: Dict) -> None:
+        self.json = body
+
+
+class MockDriverHandler(DriverHandlerInterface):
+    def standard_derivation(self, numbers):
+        return 3
+
+
+def test_calculate_integration_with_numpy():
+    """Should calculator works properly with right data"""
+    calculator = SecondCalculator(driver=NumpyHandler)
+    mocked_request = MockRequest({"numbers": [0.1, 0.2, 0.5, 0.8]})
+    response = calculator.calculate(request=mocked_request)
+    assert "data" in response
+    assert "calculator" in response["data"]
+    assert "result" in response["data"]
+    assert response["data"]["result"] == 0.38
+
+
+def test_calculate():
+    """Should calculator works properly with right data"""
+    calculator = SecondCalculator(driver=MockDriverHandler)
+    mocked_request = MockRequest({"numbers": [0.1, 0.2, 0.5, 0.8]})
+    response = calculator.calculate(request=mocked_request)
+    assert "data" in response
+    assert "calculator" in response["data"]
+    assert "result" in response["data"]
+    assert response["data"]["result"] == 0.33
+
+
+def test_calculate_without_body():
+    """
+    Should calculator raises an error when the numbers field is not present in the request body
+    """
+    calculator = SecondCalculator(driver=MockDriverHandler)
+    mocked_request = MockRequest({})
+    with pytest.raises(ValueError) as exception_info:
+        calculator.calculate(mocked_request)
+    assert (
+        str(exception_info.value)
+        == "The request body should have a field called numbers"
+    )
+
+
+def test_calculate_with_invalid_numbers_field():
+    """Should calculator raises an error when the numbers field is not a list"""
+    calculator = SecondCalculator(driver=MockDriverHandler)
+    mocked_request = MockRequest({"numbers": 12})
+    with pytest.raises(ValueError) as exception_info:
+        calculator.calculate(mocked_request)
+    assert str(exception_info.value) == "The numbers field should be a list of numbers"
+
+
+def test_calculate_with_invalid_element_in_numbers_list():
+    """Should calculator raises an error when the numbers field is not a list"""
+    calculator = SecondCalculator(driver=MockDriverHandler)
+    mocked_request = MockRequest({"numbers": [12, 13.0, "aa"]})
+    with pytest.raises(ValueError) as exception_info:
+        calculator.calculate(mocked_request)
+    assert str(exception_info.value) == "The numbers field should be a list of numbers"
