cf3192e4c0f9e456ab829a62e9e510a017bebc40
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2025-01-02 16:02:13 -0300

    feat: use the error handler to send http status code accourdingly to each error

--- Rocketseat-flask-calculator/src/main/routes/calculators.py
+++ Rocketseat-flask-calculator/src/main/routes/calculators.py
@@ -3,7 +3,7 @@
 from http import HTTPStatus, HTTPMethod
 from flask import Blueprint, request, jsonify
 from src.main.factories.calculator_factory import calculator_factory
-
+from src.errors.error_controller import handle_errors
 
 calc_routes_bp = Blueprint("calc_routes", __name__, url_prefix="/calculators")
 
@@ -11,17 +11,25 @@ calc_routes_bp = Blueprint("calc_routes", __name__, url_prefix="/calculators")
 @calc_routes_bp.route("/first", methods=[HTTPMethod.POST])
 def first_calculator():
     """This is the route controller for the first calculator problem"""
-    calculator = calculator_factory(calculator_type="first")
-    result = calculator.calculate(request=request)
-    return jsonify(result), HTTPStatus.OK
+    try:
+        calculator = calculator_factory(calculator_type="first")
+        result = calculator.calculate(request=request)
+        return jsonify(result), HTTPStatus.OK
+    except Exception as exception:
+        error_response = handle_errors(error=exception)
+        return jsonify(error_response), error_response["status_code"]
 
 
 @calc_routes_bp.route("/second", methods=[HTTPMethod.POST])
 def second_calculator():
     """This is the route controller for the second calculator problem"""
-    calculator = calculator_factory(calculator_type="second")
-    result = calculator.calculate(request=request)
-    return jsonify(result), HTTPStatus.OK
+    try:
+        calculator = calculator_factory(calculator_type="second")
+        result = calculator.calculate(request=request)
+        return jsonify(result), HTTPStatus.OK
+    except Exception as exception:
+        error_response = handle_errors(error=exception)
+        return jsonify(error_response), error_response["status_code"]
 
 
 @calc_routes_bp.route("third", methods=[HTTPMethod.POST])
@@ -29,6 +37,10 @@ def third_calculator():
     """
     This is the route controller for the third calculator problem
     """
-    calculator = calculator_factory(calculator_type="third")
-    result = calculator.calculate(request=request)
-    return jsonify(result), HTTPStatus.OK
+    try:
+        calculator = calculator_factory(calculator_type="third")
+        result = calculator.calculate(request=request)
+        return jsonify(result), HTTPStatus.OK
+    except Exception as exception:
+        error_response = handle_errors(error=exception)
+        return jsonify(error_response), error_response["status_code"]
