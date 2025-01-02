0aef5b191c1a6dd189c9488ece18a5ebe0eb7d4b
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2025-01-02 00:30:48 -0300

    feat: add interface definition for the driver responsible to calculate the standard derivation

---
+++ Rocketseat-flask-calculator/src/drivers/interfaces/driver_handler_interface.py
@@ -0,0 +1,12 @@
+"""This module contain the driver handler interface for standard derivation calculators"""
+
+from abc import ABC, abstractmethod
+from typing import List
+
+
+class DriverHandlerInterface(ABC):
+    """This is an interface for a driver responsible to calculate a standard derivation"""
+
+    @abstractmethod
+    def standard_derivation(self, numbers: List[float]) -> float:
+        """This method is responsible to calculate the standard derivation of a list of numbers"""
